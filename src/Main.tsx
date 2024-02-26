e1a5d2291c37bd60ff4beb9312add5b434c56a17
Lopakadude <lopakadude@gmail.com>
2024-02-26 15:26:50 +0300

    add components

---
+++ src/Main.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App';
+import './index.css';
+import '@gravity-ui/uikit/styles/fonts.css';
+import '@gravity-ui/uikit/styles/styles.css';
+import { ThemeProvider } from '@gravity-ui/uikit';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ThemeProvider theme="light">
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+);
