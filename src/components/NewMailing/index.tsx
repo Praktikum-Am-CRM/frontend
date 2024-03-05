ad63bb703dbb520a07d87dbf6ddeff68733b6428
iammacheta <iammacheta@gmail.com>
2024-03-05 22:36:16 +0100

    feat: добавить выбор даты отправки отложенного

--- src/components/NewMailing/index.tsx
+++ src/components/NewMailing/index.tsx
@@ -1,8 +1,11 @@
 /* eslint-disable no-console */
 import styles from './styles.module.css';
-import { Button, ButtonView, TextArea } from '@gravity-ui/uikit';
+import { Button, ButtonView, Modal, TextArea } from '@gravity-ui/uikit';
+import type { DateTime } from '@gravity-ui/date-utils';
+import { DatePicker } from '@gravity-ui/date-components';
 import { useAppSelector } from '../../hooks/redux';
 import { useActions } from '../../hooks/actions';
+import { useState } from 'react';
 
 const NewMailing = () => {
   const textAreaValue = useAppSelector(state => state.mailing.textAreaValue);
@@ -10,6 +13,9 @@ const NewMailing = () => {
   const selectedUsersIds = useAppSelector(
     state => state.table.selectedUsersIds,
   );
+
+  const [datePickerOpened, setDatePickerOpened] = useState(false);
+  const [pickedDate, setPickedDate] = useState(new Date().toISOString());
 
   const handleTextAreaChange = (
     event: React.ChangeEvent<HTMLTextAreaElement>,
@@ -27,12 +33,40 @@ const NewMailing = () => {
     console.log(`Отправка текста : ${textAreaValue} `);
   };
 
+  const handlePostponedClick = () => {
+    setDatePickerOpened(true);
+  };
+
   const isButtonActive = () => {
     return textAreaValue.trim() === '';
   };
 
+  const handlePickDate = (value: DateTime | null) => {
+    if (value) {
+      setPickedDate(value.toISOString());
+    } else {
+      setPickedDate('');
+    }
+  };
+
+  const handleSendButtonClicked = (value: string) => {
+    console.log(value);
+    setDatePickerOpened(false);
+  };
+
+  function formatDate(dateISO: string) {
+    const dateObj = new Date(dateISO);
+    const options: Intl.DateTimeFormatOptions = {
+      month: 'long',
+      year: 'numeric',
+      day: '2-digit',
+    };
+    const formattedDate = dateObj.toLocaleDateString('ru-RU', options);
+    return formattedDate;
+  }
+
   const createButton = (
-    text: string,
+    content: React.ReactNode,
     onClick: React.MouseEventHandler<HTMLButtonElement | HTMLAnchorElement>,
     view: ButtonView = 'normal',
   ) => (
@@ -43,33 +77,55 @@ const NewMailing = () => {
       onClick={onClick}
       disabled={isButtonActive()}
     >
-      {text}
+      {content}
     </Button>
   );
 
   return (
     <>
-      <Button className={styles.templatesButton} size="l">
-        Шаблоны
-      </Button>
-      <TextArea
-        placeholder="Начните новую рассылку"
-        size="l"
-        className={styles.textArea}
-        value={textAreaValue}
-        onChange={handleTextAreaChange}
-      />
+      <Modal open={datePickerOpened} onClose={() => setDatePickerOpened(false)}>
+        <div className={styles.modal}>
+          <DatePicker
+            placeholder="Выберите дату отправки"
+            size="xl"
+            style={{ width: '300px' }}
+            onUpdate={handlePickDate}
+          />
+          <Button
+            disabled={Object.keys(pickedDate).length === 0}
+            size="xl"
+            onClick={() => handleSendButtonClicked(pickedDate)}
+          >
+            {`Отправить ${
+              Object.keys(pickedDate).length > 0 ? formatDate(pickedDate) : ''
+            }`}
+          </Button>
+        </div>
+      </Modal>
+      <>
+        <Button className={styles.templatesButton} size="l">
+          Шаблоны
+        </Button>
+        <TextArea
+          placeholder="Начните новую рассылку"
+          size="l"
+          className={styles.textArea}
+          value={textAreaValue}
+          onChange={handleTextAreaChange}
+        />
 
-      <div className={styles.actions}>
-        {createButton(
-          `Отправить выбранным (${selectedUsersIds.length > 0 ? selectedUsersIds.length : 0})`,
-          handleSendToSelected,
-          'action',
-        )}
-        {createButton('Отправить всем', handleSendClick)}
-        {createButton('Отложить рассылку', handleSendClick)}
-        {createButton('В черновики', handleSendClick)}
-      </div>
+        <div className={styles.actions}>
+          {createButton(
+            `Отправить выбранным (${selectedUsersIds.length > 0 ? selectedUsersIds.length : 0})`,
+            handleSendToSelected,
+            'action',
+          )}
+          {createButton('Отправить всем', handleSendClick)}
+
+          {createButton('Отложить рассылку', handlePostponedClick)}
+          {createButton('В черновики', handleSendClick)}
+        </div>
+      </>
     </>
   );
 };
