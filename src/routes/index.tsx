d0b85133eb431bff1e20fcb30a5ef3b18912abd7
iammacheta <iammacheta@gmail.com>
2024-03-06 17:23:42 +0100

    fix: вынести загрузку компонентов для lazy в отдельный файл

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,21 @@
-import { lazy } from 'react';
 import {
   Route,
   createBrowserRouter,
   createRoutesFromElements,
 } from 'react-router-dom';
 
+import {
+  AmbassadorsPage,
+  CandidatesPage,
+  LoginPage,
+  MerchPage,
+  NotFoundPage,
+  NotificationsPage,
+  StatisticsPage,
+} from './components';
+
 import Layout from '../components/Layout';
 import ProtectedRoute from '../components/ProtectedRoute';
-
-const AmbassadorsPage = lazy(() => import('../pages/AmbassadorsPage'));
-const CandidatesPage = lazy(() => import('../pages/CandidatesPage'));
-const MerchPage = lazy(() => import('../pages/MerchPage'));
-const NotFoundPage = lazy(() => import('../pages/NotFoundPage'));
-const NotificationsPage = lazy(() => import('../pages/NotificationsPage'));
-const StatisticsPage = lazy(() => import('../pages/StatisticsPage'));
-const LoginPage = lazy(() => import('../pages/LoginPage'));
 
 const router = createBrowserRouter(
   createRoutesFromElements(
