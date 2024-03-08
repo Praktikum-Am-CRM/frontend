4a07e5ce2e2943d0a357ee3f3cf4846dc9d374b6
Constantine <asbest911@gmail.com>
2024-03-09 00:13:37 +0300

    feat: add merchTable

--- src/components/MerchTable/index.tsx
+++ src/components/MerchTable/index.tsx
@@ -1,79 +1,87 @@
 import { Label, Loader, Table, withTableSorting } from '@gravity-ui/uikit';
 import { useGetMerchRequestsQuery } from '../../store/amCrm/amCrm.api';
 import styles from './styles.module.css';
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 import { MerchRequestListType } from '../../types/types';
 import { TextWithTooltip } from '../TextWithTooltip';
+import ModalWindow from '../ModalWindow';
+import Card from '../Card';
+import { useActions } from '../../hooks/actions';
+import { useAppSelector } from '../../hooks/redux';
+
+const MyTable = withTableSorting(Table);
+
+const columns = [
+  {
+    id: 'status',
+    name: 'Статус',
+    width: 200,
+    meta: {
+      defaultSortOrder: 'desc',
+      sort: (
+        a: { status: { props: { children: string } } },
+        b: { status: { props: { children: string } } },
+      ) => {
+        if (a.status.props.children < b.status.props.children) {
+          return -1;
+        }
+        if (a.status.props.children > b.status.props.children) {
+          return 1;
+        }
+        return 0;
+      },
+    },
+  },
+  {
+    id: 'merchName',
+    name: 'Мерч',
+    width: 200,
+    meta: {
+      defaultSortOrder: 'desc',
+      sort: (a: { merchName: string }, b: { merchName: string }) => {
+        if (a.merchName < b.merchName) {
+          return -1;
+        }
+        if (a.merchName > b.merchName) {
+          return 1;
+        }
+        return 0;
+      },
+    },
+  },
+  { id: 'size', name: 'Размер' },
+  {
+    id: 'ambassador',
+    name: `Амбассадор`,
+  },
+  { id: 'address', name: 'Адрес' },
+  { id: 'tel', name: 'Телефон' },
+  { id: 'сomment', name: 'Комментарий', width: 300 },
+];
+
+function defineStatusLabel(status: string) {
+  switch (status) {
+    case 'мерч направлен':
+      return 'success';
+    case 'на подтверждении адреса':
+      return 'warning';
+    case 'адрес подтвержден':
+      return 'info';
+    default:
+      return 'unknown';
+  }
+}
 
 export default function MerchTable() {
+  const [ambId, setAmbId] = useState<string>('');
   const { data: merch, isFetching } = useGetMerchRequestsQuery();
 
-  const MyTable = withTableSorting(Table);
-
-  const columns = [
-    {
-      id: 'status',
-      name: 'Статус',
-      width: 200,
-      meta: {
-        defaultSortOrder: 'desc',
-        sort: (
-          a: { status: { props: { children: string } } },
-          b: { status: { props: { children: string } } },
-        ) => {
-          if (a.status.props.children < b.status.props.children) {
-            return -1;
-          }
-          if (a.status.props.children > b.status.props.children) {
-            return 1;
-          }
-          return 0;
-        },
-      },
-    },
-    {
-      id: 'merchName',
-      name: 'Мерч',
-      width: 200,
-      meta: {
-        defaultSortOrder: 'desc',
-        sort: (a: { merchName: string }, b: { merchName: string }) => {
-          if (a.merchName < b.merchName) {
-            return -1;
-          }
-          if (a.merchName > b.merchName) {
-            return 1;
-          }
-          return 0;
-        },
-      },
-    },
-    { id: 'size', name: 'Размер' },
-    {
-      id: 'ambassador',
-      name: `Амбассадор`,
-    },
-    { id: 'address', name: 'Адрес' },
-    { id: 'tel', name: 'Телефон' },
-    { id: 'сomment', name: 'Комментарий', width: 300 },
-  ];
-
-  function defineStatusLabel(status: string) {
-    switch (status) {
-      case 'мерч направлен':
-        return 'success';
-      case 'на подтверждении адреса':
-        return 'warning';
-      case 'адрес подтвержден':
-        return 'info';
-      default:
-        return 'unknown';
-    }
-  }
+  const { openModal } = useActions();
+  const isModalOpen = useAppSelector(state => state.modal.isModalOpen);
 
   const prepareDataForTable = useCallback((data: MerchRequestListType) => {
     return {
-      id: data.id,
+      id: data.ambassadors.id,
       status: (
         <Label theme={defineStatusLabel(data.request_status.status_name)}>
           {data.request_status.status_name}
@@ -97,6 +105,13 @@ export default function MerchTable() {
     };
   }, []);
 
+  function handleClick(id: string) {
+    setAmbId(id);
+    openModal();
+  }
+
+  const content = <Card rowId={ambId} isAmbassador />;
+
   return (
     <div>
       {isFetching ? (
@@ -104,9 +119,9 @@ export default function MerchTable() {
       ) : (
         merch && (
           <MyTable
-            className={styles.table}
+            className={styles.merchTable}
             onRowClick={evt => {
-              console.log(evt);
+              handleClick(evt.id);
             }}
             emptyMessage="Ничего не найдено ¯\_(ツ)_/¯"
             data={merch.map(prepareDataForTable)}
@@ -114,6 +129,7 @@ export default function MerchTable() {
           />
         )
       )}
+      {isModalOpen && <ModalWindow content={content} />}
     </div>
   );
 }
