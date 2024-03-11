27522f7aa405c1164a88e1bb31b7b62ce725f0a1
iammacheta <iammacheta@gmail.com>
2024-03-11 10:27:38 +0100

    feat: прикрутить ручку бэка к истории сообщений

--- src/components/Chat/index.tsx
+++ src/components/Chat/index.tsx
@@ -1,13 +1,26 @@
 import styles from './styles.module.css';
 import CommunicationHistory from '../CommunicationHistory';
 import PersonalMessage from '../PersonalMessage';
+import { Spin } from '@gravity-ui/uikit';
+import { useGetAmbassadorMessagesQuery } from '../../store/amCrm/amCrm.api';
+import { useAppSelector } from '../../hooks/redux';
 
 const Chat = () => {
-  return (
-    <div className={styles.root}>
-      <CommunicationHistory />
-      <PersonalMessage />
-    </div>
+  const pickedRowUserId = useAppSelector(state => state.table.pickedRowUserId);
+
+  const { data: messageHistoryAmbassadorId, isFetching } =
+    useGetAmbassadorMessagesQuery({
+      id: pickedRowUserId,
+    });
+  return isFetching ? (
+    <Spin className={styles.spin} size="xl" />
+  ) : (
+    messageHistoryAmbassadorId && (
+      <div className={styles.root}>
+        <CommunicationHistory messages={messageHistoryAmbassadorId} />
+        <PersonalMessage />
+      </div>
+    )
   );
 };
 
