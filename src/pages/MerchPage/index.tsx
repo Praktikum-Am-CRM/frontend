f6f26a584cd4589a809f82a79a42dfb9617d6be7
Constantine <asbest911@gmail.com>
2024-03-11 07:11:15 +0300

    feat: add merch costs

--- src/pages/MerchPage/index.tsx
+++ src/pages/MerchPage/index.tsx
@@ -1,11 +1,14 @@
 import { Button, Tabs } from '@gravity-ui/uikit';
 import MerchTable from '../../components/MerchTable';
 import { useState } from 'react';
+import { useActions } from '../../hooks/actions';
+import ModalWindow from '../../components/ModalWindow';
+import Costs from '../../components/Costs';
 
 export default function MerchPage() {
   const [activeTab, setActiveTab] = useState<string>('merch');
   const [isMerch, setIsMerch] = useState<boolean>(true);
-
+  const { openModal } = useActions();
   function handleTabClick(id: string) {
     setActiveTab(id);
     setIsMerch(id === 'merch');
@@ -30,15 +33,17 @@ export default function MerchPage() {
             }}
           /> */}
         </Tabs>
-        <Button view="action" size="l">
+        <Button view="action" size="l" onClick={() => openModal()}>
           Цены на мерч
         </Button>
       </div>
+
       {isMerch ? (
         <MerchTable />
       ) : (
         <div>Тут расходы на мерч на каждого амбассадора по месяцам</div>
       )}
+      <ModalWindow content={<Costs />} />
     </>
   );
 }
